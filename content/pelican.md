7636346cc823856c75f3bdf3a41f5b8fb41a800d
Guilherme Ferreira <360guilherme@gmail.com>
2017-01-27 15:19:55 -0300

    Pelican blog sources

---
+++ content/pelican.md
@@ -0,0 +1,27 @@
+Title: Criando um blog estatico com Pelican
+Date: 2017-01-23 17:20
+Category: CMS
+Tags: CMS, Pelican
+
+[Pelican](https://github.com/getpelican/pelican) é um gerador de sites estaticos, escrito em Python.
+
+Sua instalação e execução é bastante simples e depende de poucas linhas de comando.
+
+#Instalação e configuraçao do Pelican
+	pip3 install pelican markdown
+	pelican-quickstart 
+
+#Execução do pelican em abiente de local
+	cd output/
+	python3 -m pelican.server
+
+#Criação de conteúdo
+Para criar conteudo, basta criar arquivos de texto dentro do diretório content/ utilizando a extenção .md ([markdown](http://daringfireball.net/projects/markdown/) )
+Os metadados devem ser informado no inicio do arquivo como o exemplo abaixo:
+
+	Title: Criando um blog estatico com Pelican
+	Date: 2017-01-23 17:20
+	Category: CMS
+
+Depois basta rodar o comando 
+	pelican content
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Guilherme Ferreira'
+SITENAME = 'Guilherme Dev Blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Araguaina'
+
+DEFAULT_LANG = 'pt'
+
+THEME = 'blue-penguin-master'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/Guilh_rm_'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
